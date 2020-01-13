169f95b26411d22121095bd7cf6b1cde67d92330
mattew8702 <gatti.matteo@iisgalvanimi.edu.it>
2020-01-13 14:08:30 +0000

    Creazione macchina virtuale

---
+++ angular-hello-world/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ComponenteComponent } from './componente/componente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponenteComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
