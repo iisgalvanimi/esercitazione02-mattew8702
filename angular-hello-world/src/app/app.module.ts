4b7391b50040bee08cb74c6a3ca845e7f522cb9a
mattew8702 <gatti.matteo@iisgalvanimi.edu.it>
2020-01-14 11:53:33 +0000

    aggiunta componente prova2

--- angular-hello-world/src/app/app.module.ts
+++ angular-hello-world/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ComponenteComponent } from './componente/componente.component';
+import { Componente2Component } from './componente2/componente2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponenteComponent
+    ComponenteComponent,
+    Componente2Component
   ],
   imports: [
     BrowserModule
